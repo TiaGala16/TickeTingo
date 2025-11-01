36973f8c8a70f4025f326b1288df57d7b22992ab
khushi <shahkhushi9014@gmail.com>
2025-11-02 00:12:26 +0530

    soldTix<totalTix validation done

--- app/src/main/java/com/example/ticketingo/viewmodel/EventViewModel.java
+++ app/src/main/java/com/example/ticketingo/viewmodel/EventViewModel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.ticketingo.model.Event;
@@ -12,23 +13,39 @@ import com.example.ticketingo.model.EventRepo;
 import java.util.List;
 
 public class EventViewModel extends ViewModel {
-    private final EventRepo repo=new EventRepo();
-
-    public void createEvent(Context context, String title, String description,String time,String organiser,
+    private final EventRepo repo = new EventRepo();
+    private final MutableLiveData<String> errorLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Boolean> soldOutStatus = new MutableLiveData<>();
+    public void createEvent(Context context, String title, String description, String time, String organiser,
                             String date, double price, int totalTickets, Uri imageUri, String location) {
-        repo.createEvent(context, title, description,time, organiser,date, price, totalTickets, imageUri, location);
+        repo.createEvent(context, title, description, time, organiser, date, price, totalTickets, imageUri, location);
     }
-
-    public void loadEvent(String eventName){
+    public void checkIfEventSoldOut(String eventTitle) {
+        repo.checkIfEventSoldOut(eventTitle, soldOutStatus, errorLiveData);
+    }
+    public void loadEvent(String eventName) {
         repo.loadEvent(eventName);
     }
-    public void loadEvents(){
+    public void loadEvents() {
         repo.loadEvents();
     }
-    public LiveData<Boolean> getUploadStatus() { return repo.getUploadStatus(); }
-    public LiveData<String> getError() { return repo.getErrorLiveData(); }
-    public LiveData<List<Event>> getEvents(){ return repo.getEventsLiveData();}
     public void loadEventsByOrganiser(String organiserName) {
         repo.loadEventsByOrganiser(organiserName);
+    }
+
+    public LiveData<List<Event>> getEvents() {
+        return repo.getEventsLiveData();
+    }
+
+    public LiveData<Boolean> getUploadStatus() {
+        return repo.getUploadStatus();
+    }
+
+    public LiveData<String> getError() {
+        return errorLiveData;
+    }
+
+    public LiveData<Boolean> getSoldOutStatus() {
+        return soldOutStatus;
     }
 }
